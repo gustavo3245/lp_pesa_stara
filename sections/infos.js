75910c24291103a8572966302a8a090dd06371e4
Gabriel Cardoso <68553837+GabrielCardoso-psd@users.noreply.github.com>
2022-05-27 17:31:27 -0300

    counter

--- sections/infos.js
+++ sections/infos.js
@@ -1,8 +1,12 @@
 import css from './infos.module.css'
 import { Row, Col, Container } from 'reactstrap'
 import Image from "next/image";
+import React, {useState} from "react"
+import AnimatedNumber from 'react-animated-number';
+import CountUp, { useCountUp } from 'react-countup';
 
 export default function Infos (){
+    const [counter, setCounter]=useState(0)
     return(
         <div className={css.background}>
             <style>
@@ -13,8 +17,22 @@ export default function Infos (){
                     <Col>
                     <Image className={css.image} src="/imgs/icon-1.png" width={91} height={91} alt="icon"/>
                     <div className={css.title}>O agronegócio <br/> é</div>
-                    <div className={css.value}>27,4%</div>
+                    <div className={css.value}>
+                    {/* <AnimatedNumber 
+                    value={counter}
+                    style={
+                        {
+                            fontSize: 70
+                        }
+                    }
+                    formatValue={n=>n.toFixed(0) }
+                    frameStyle={percentage=>percentage>20 && percentage<80 ? {opacity: 0.5}:{}}
+                    /> */}
+                    <CountUp end={27} duration={4}/>,<CountUp end={4} duration={4}/>%</div>
                     <div className={css.title2}>do PIB do Brasil*</div>
+                    {/* <div className='area'>
+                        <button onClick={()=>setCounter(counter + 27)}></button>
+                    </div> */}
                     </Col>
 
                     <Col>
@@ -24,14 +42,14 @@ export default function Infos (){
                         <span style={{fontFamily: "Raleway", fontSize: "35px"}}>
                             R$
                             </span>
-                             1,98</div>
+                            <CountUp end={1} duration={4}/>,<CountUp end={98} duration={4}/></div>
                     <div className={css.title2}>trilhão*</div>
                     </Col>
 
                     <Col>
                     <Image className={css.image} src="/imgs/icon-3.png" width={91} height={91} alt="icon"/>
                     <div className={css.title}>O agronegócio<br/>teve um superavit de</div>
-                    <div className={css.value}><span style={{fontFamily: "Raleway", fontSize: "35px"}}>U$</span>102,1</div>
+                    <div className={css.value}><span style={{fontFamily: "Raleway", fontSize: "35px"}}>U$</span><CountUp end={102} duration={4}/>,<CountUp end={1} duration={4}/></div>
                     <div className={css.title2}>bilhões em 2021**</div>
                     </Col>
                 </Row>
