178d15d761bf6851850c2763c5d29304cee35d0a
Matheus Henrique <40078286+fabiodevn1@users.noreply.github.com>
2022-05-30 12:10:38 -0300

    update stara

--- sections/infos.js
+++ sections/infos.js
@@ -2,39 +2,32 @@ import css from './infos.module.css'
 import { Row, Col, Container } from 'reactstrap'
 import Image from "next/image";
 import React, {useState} from "react"
-import AnimatedNumber from 'react-animated-number';
-import CountUp, { useCountUp } from 'react-countup';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
 export default function Infos (){
-    const [counter, setCounter]=useState(0)
+
+
+    function onChange() {
+        console.log('estou aqui')
+    }
+
     return(
         <div className={css.background}>
             <style>
 @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,500&display=swap');
 </style>
+
             <Container>
                 <Row className={css.row}>
                     <Col>
                     <Image className={css.image} src="/imgs/icon-1.png" width={91} height={91} alt="icon"/>
                     <div className={css.title}>O agronegócio <br/> é</div>
-                    <div className={css.value}>
-                    {/* <AnimatedNumber 
-                    value={counter}
-                    style={
-                        {
-                            fontSize: 70
-                        }
-                    }
-                    formatValue={n=>n.toFixed(0) }
-                    frameStyle={percentage=>percentage>20 && percentage<80 ? {opacity: 0.5}:{}}
-                    /> */}
-                    <CountUp end={27} duration={4}/>,<CountUp end={4} duration={4}/>%</div>
+                    <div>
+                        <ContadorAgro2 numero1={27} numero2={4}/>
+                    </div>
                     <div className={css.title2}>do PIB do Brasil*</div>
-                    {/* <div className='area'>
-                        <button onClick={()=>setCounter(counter + 27)}></button>
-                    </div> */}
                     </Col>
-
                     <Col>
                     <Image className={css.image} src="/imgs/icon-2.png" width={91} height={91} alt="icon"/>
                     <div className={css.title}>O agronegócio <br/>gerou</div>
@@ -42,14 +35,18 @@ export default function Infos (){
                         <span style={{fontFamily: "Raleway", fontSize: "35px"}}>
                             R$
                             </span>
-                            <CountUp end={1} duration={4}/>,<CountUp end={98} duration={4}/></div>
+
+                    </div>
                     <div className={css.title2}>trilhão*</div>
                     </Col>
 
                     <Col>
                     <Image className={css.image} src="/imgs/icon-3.png" width={91} height={91} alt="icon"/>
                     <div className={css.title}>O agronegócio<br/>teve um superavit de</div>
-                    <div className={css.value}><span style={{fontFamily: "Raleway", fontSize: "35px"}}>U$</span><CountUp end={102} duration={4}/>,<CountUp end={1} duration={4}/></div>
+                    <div>
+                        <span style={{fontFamily: "Raleway", fontSize: "35px"}}>U$</span>
+                        <ContadorAgro2 numero1={102} numero2={1}/>
+                    </div>
                     <div className={css.title2}>bilhões em 2021**</div>
                     </Col>
                 </Row>
@@ -63,5 +60,39 @@ export default function Infos (){
                 </Row>
             </Container>
         </div>
+    )
+}
+
+function ContadorAgro2({numero1, numero2}) {
+    return (
+        <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+            {({ isVisible }) => (
+                <div style={{ height: 100 }}>
+                    {isVisible ?
+                        <div className={css.value}>
+                            <CountUp end={numero1} duration={4}/>,
+                            <CountUp end={numero2} duration={4}/>%
+                        </div>: null}
+                </div>
+            )}
+        </VisibilitySensor>
+    )
+}
+
+
+function ContadorAgro3({numero1, numero2}) {
+    return (
+        <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+            {({ isVisible }) => (
+                <div style={{ height: 100 }}>
+                    {isVisible ?
+                        <div className={css.value}>
+                            $
+                            <CountUp end={numero1} duration={4}/>,
+                            <CountUp end={numero2} duration={4}/>%
+                        </div>: null}
+                </div>
+            )}
+        </VisibilitySensor>
     )
 }
